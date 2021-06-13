3a34d8e786c1a3c4ec74c6d78705a1077c1a5311
N-pacis <pacisnkubito@gmail.com>
2021-06-13 22:19:05 +0200

    added navbar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,26 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import {RouterModule} from '@angular/router';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CompetitionsComponent } from './competitions/competitions.component';
+import { NavbarComponentComponent } from './navbar-component/navbar-component.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CompetitionsComponent,
+    NavbarComponentComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot([
+      {
+        path:'competitions',
+        component: CompetitionsComponent
+      }])
   ],
   providers: [],
   bootstrap: [AppComponent]
