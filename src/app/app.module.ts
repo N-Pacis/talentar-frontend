e2c5f2d011e2b8ff3050c51353d66d32909cddea
NickSingizwa <singizwanick19@gmail.com>
2021-06-15 22:11:39 +0200

    pages init

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,22 +7,53 @@ import { AppComponent } from './app.component';
 import { CompetitionsComponent } from './components/competitions/competitions.component';
 import { NavbarComponentComponent } from './components/navbar-component/navbar-component.component';
 import { AllpageComponent } from './components/allpage/allpage.component';
+import { PhotographyComponent } from './components/photography/photography.component';
+import { ArtComponent } from './components/art/art.component';
+import { DesignComponent } from './components/design/design.component';
+import { SingingComponent } from './components/singing/singing.component';
+import { PostcardComponent } from './components/postcard/postcard.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     CompetitionsComponent,
     NavbarComponentComponent,
-    AllpageComponent
+    AllpageComponent,
+    PhotographyComponent,
+    ArtComponent,
+    DesignComponent,
+    SingingComponent,
+    PostcardComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     RouterModule.forRoot([
       {
-        path:'competitions',
+        path: 'competitions',
         component: CompetitionsComponent
-      }])
+      },
+      {
+        path: 'all',
+        component: AllpageComponent
+      },
+      {
+        path: 'photography',
+        component: PhotographyComponent
+      },
+      {
+        path: 'art',
+        component: ArtComponent
+      },
+      {
+        path: 'design',
+        component: DesignComponent
+      },
+      {
+        path: 'singing',
+        component: SingingComponent
+      },
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
