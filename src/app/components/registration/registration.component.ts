d6712f9393a9094f4b4408f37d76645aa8c821a2
I-Clarisse <79416565+I-Clarisse@users.noreply.github.com>
2021-06-24 18:01:55 +0200

    registration

---
+++ src/app/components/registration/registration.component.ts
@@ -0,0 +1,24 @@
+import { Component, OnInit } from '@angular/core';
+import {Router} from '@angular/router';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+
+@Component({
+  selector: 'app-registration',
+  templateUrl: './registration.componen t.html',
+  styleUrls: ['./registration.component.css']
+})
+export class RegistrationComponent{
+  // registerForm: FormGroup;
+  // loading = false;
+  // submitted = false;
+
+  constructor() { 
+    // this.registerForm;
+    // private formBuilder: FormBuilder,
+    // private router: Router
+  }
+
+   submit(){
+     console.log('Form Submitted');
+   }
+}
